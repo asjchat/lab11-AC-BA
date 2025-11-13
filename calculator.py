92697c8cf3cb6b0aa1644a52064db177459267c0
asjchat <asjchat@gmail.com>
2025-11-12 19:38:28 -0500

    merged changes

--- calculator.py
+++ calculator.py
@@ -5,7 +5,9 @@ calculator.py
 One function per operation, in order.
 """
 # First example
+
 import math
+
 def add(a, b):
     return a + b
 
@@ -28,7 +30,6 @@ def log(a, b):
 def exp(a, b):
     return a**b
 
-import math
 
 def square_root(a):
     if a < 0:
@@ -38,23 +39,5 @@ def square_root(a):
 def hypotenuse(a, b):
     math.hypot(a, b) # can have negative nums
 
-def add(a, b): 
-    pass
-
-def sub(a, b):
-    return a - b
-
-def mul(a, b):
-    return a * b
-
-def log(a, b):
-    if a <= 0 or a == 1:
-        raise ValueError("Error")
-    if b <= 0:
-        raise ValueError("Error")
-    return math.log(b, a)
-
-def exp(a, b):
-    return a ** b
 
 
